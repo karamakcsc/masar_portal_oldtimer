c7521246ac187931e4947a0152742ec34f31e066
karamakcsc <karamakcsc@hotmail.com>
2022-06-23 15:02:28 +0000

    upload all pages

--- masar_portal_oldtimer/www/oldtimer/category.py
+++ masar_portal_oldtimer/www/oldtimer/category.py
@@ -1,13 +1,12 @@
 import frappe
-
 def get_context(context):
 	#Banners Loop
 	item = frappe.db.sql(""" SELECT ti.name, ti.item_name, ti.item_code, ti.description, ti.image, ti.disabled, tip.price_list, price_list_rate
 							FROM `tabItem` ti
 							inner JOIN `tabItem Price` tip
-                 			ON (tip.item_code = ti.item_code AND tip.brand =ti.brand)
+							ON (tip.item_code = ti.item_code AND tip.brand =ti.brand)
 							WHERE ti.disabled= 0 AND ti.brand = 'Baja Designs' AND tip.price_list = 'BajaDesigns MSRP -USD'
-						    ORDER BY ti.creation DESC LIMIT 12;""", as_dict=True)
+							ORDER BY ti.creation DESC LIMIT 12;""", as_dict=True)
 	context.item = item
 
 	#Second Category Loop
@@ -16,9 +15,8 @@ def get_context(context):
 	context.cat_nd = cat_nd
 
 	#Thired Category Loop
-	cat_th = frappe.db.sql(""" SELECT name, creation, first_category, second_category, third_category
-						   FROM `tabItem Third Category`  WHERE first_category ="Cars" AND second_category= {context.cat_nd.second_category} ORDER BY creation DESC LIMIT 12;""", as_dict=True)
-	context.cat_th = cat_th
-
+	# cat_th = frappe.db.sql(""" SELECT name, creation, first_category, second_category, third_category
+	# 					   FROM `tabItem Third Category`  WHERE first_category ="Cars" AND second_category= {context.cat_nd.second_category} ORDER BY creation DESC LIMIT 12;""", as_dict=True)
+	# context.cat_th = cat_th
 
 	return context
